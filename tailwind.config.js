54c201e8a8114f07f3b5af17ea76c884b8073195
mohammad <mohammadtaheri30132@gmail.com>
2022-07-11 21:55:56 +0430

    base Project

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#0e2338',
+        'main-second':'#162f48',
+        'text':'#334155 ',
+        'text-second':'#334155',
+        'text-active':'#51B6ff',
+        'text-nav':'#ffffff',
+        'active-nav':'#112943',
+        'danger':'#DC2626',
+        'disable':'#A5ACbb',
+        'btn':'#3b82F6',
+
+
+
+
+      },
+    },
+
+
+  },
+  plugins: [],
+}
